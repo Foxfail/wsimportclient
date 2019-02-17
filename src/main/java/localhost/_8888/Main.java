b940ebc181abf735d43c628b90eec610e773ffff
Foxfail <foxfail@mail.ru>
2019-02-17 14:11:36 +0300

    Добавил опрос клиентом сервиса

--- src/main/java/localhost/_8888/Main.java
+++ src/main/java/localhost/_8888/Main.java
@@ -1,19 +1,55 @@
 package localhost._8888;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
-    public static void main(String[] args) {
+
+    private static int number_of_requests = 100;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("start");
         AsyncImplService asyncImplService = new AsyncImplService();
+        System.out.println("connecting...");
         AsyncImpl asyncImpl = asyncImplService.getAsyncImplPort();
+        System.out.println("sending 100 requests");
         ArrayList<Integer> ids = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+
+        // ЗАПРАШИВАЕМ ИНФУ
+        for (int i = 0; i < number_of_requests; i++) {
             Integer id = asyncImpl.addDataRequest("INN");
             ids.add(id);
-            System.out.println(id);
+
+            // вывожу красиво на экран
+            if (i % 20 == 0) {
+                System.out.println();
+            }
+            System.out.print(" " + id + " ");
+        }
+        System.out.println();
+
+
+        Thread.sleep(1000);
+
+        // ЗАПРАШИВАЕМ РЕЗУЛЬТАТЫ
+        ArrayList<String> resultsArrayList = new ArrayList<>();
+
+        Iterator<Integer> iterator = ids.iterator();
+        while (iterator.hasNext()) {
+
+            String response = asyncImpl.pollForResult(iterator.next());
+            if (response != null) {
+                iterator.remove();
+                resultsArrayList.add(response);
+
+            }
+            resultsArrayList.trimToSize();
+            if (!iterator.hasNext()){
+                iterator = ids.iterator();
+            }
         }
 
 
-
+        System.out.println(resultsArrayList.toString());
     }
 }
